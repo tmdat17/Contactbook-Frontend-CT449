aede2716099c2e23ded36e756266d0276d4c1b67
Dat17 <tmdatsc.vl@gmail.com>
2022-10-22 16:35:01 +0700

    "Hien thi danh sach cac contact lay ve tu server"

---
+++ src/components/ContactCard.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+    props: {
+        contact: {type: Object, require: true},
+    },
+};
+</script>
+
+<template>
+    <div>
+        <div class="p-1">
+            <strong>Tên:</strong>
+            {{ Contact.name }}
+        </div>
+        <div class="p-1">
+            <strong>E-mail:</strong>
+            {{ Contact.email }}
+        </div>
+        <div class="p-1">
+            <strong>Địa chỉ:</strong>
+            {{ Contact.address }}
+        </div>
+        <div class="p-1">
+            <strong>Điện thoại:</strong>
+            {{ Contact.phone }}
+        </div>
+        <div class="p-1">
+            <strong>Liên hệ yêu thích: &nbsp;</strong>
+            <i 
+                v-if="contact.favorite"
+                class="fas fa-check"
+            ></i>
+
+            <i
+                v-else
+                class="fas fa-times"
+            ></i>
+        </div>
+    </div>
+</template>
+
