8212f57acae867e09237e730f12104c3ad75a1d0
Dat17 <tmdatsc.vl@gmail.com>
2022-11-01 17:14:47 +0700

    Update ContactAdd.vue

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,34 @@
+<template>
+  <h4>Thêm liên hệ mới</h4>
+  <ContactForm :contact="contact" @submit:contact="createContact" />
+  <p>{{ message }}</p>
+</template>
+
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+export default {
+  components: {
+    ContactForm,
+  },
+  data() {
+    return {
+      contact: {},
+      message: "",
+    };
+  },
+  methods: {
+    async createContact() {
+      try {
+        await ContactService.create(this.contact);
+        this.message = "Liên hệ được thêm thành công";
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
